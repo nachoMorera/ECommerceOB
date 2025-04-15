de6b27bc1b9c2b55c05703264099fc082ebe62ca
“nachoMorera” <ijmorera19@gmail.com>
2025-04-15 09:59:58 +0200

    main entry point for the Spring Boot application

---
+++ src/main/java/org/example/ecommerceob/ECommerceObApplication.java
@@ -0,0 +1,15 @@
+package org.example.ecommerceob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class ECommerceObApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceObApplication.class, args);
+    }
+
+}
